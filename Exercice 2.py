4426d1c50b538d43b078e2dced0c5b6713ad1d85
Guittet <og67guittet@gmail.com>
2023-11-07 09:59:01 +0100

    R309

---
+++ Exercice 2.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    try:
+        file_name = "a.txt"
+        with open(file_name, 'r') as f:
+            for l in f:
+                l = l.rstrip("\n\r")
+                print(l)
+    except FileNotFoundError:
+        print(f"le fichier {file_name} n'existe pas.")
+    except IOError:
+        print(f"le fichier n'arrive pas a lire {file_name}.")
+    except FileExistsError:
+        print(f"le fichier {file_name} existe deja.")
+    except PermissionError:
+        print(f"permission non autoriser pour le fichier {file_name}.")
