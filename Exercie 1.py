030b3b4778f8f92441dd8b9cfbec14c9eb55f077
Guittet <og67guittet@gmail.com>
2023-11-07 09:36:16 +0100

    R309

--- Exercie 1.py
+++ Exercie 1.py
@@ -1,15 +1,19 @@
-
 def divEntier(x: int, y: int) -> int:
- if x < y:
-    return 0
- else:
-    x = x - y
-    return divEntier(x, y) + 1
- 
+    if x < 0 or y < 0:
+        raise ValueError("les nombres doivent être positifs")
+    elif y == 0:
+        raise ValueError("Y doit être différent de 0")
+    else:
+        if x < y:
+            return 0
+        else:
+            x = x - y
+            return divEntier(x, y) + 1
+
 if __name__ == '__main__':
     try:
-         x = int(input("a: ")) 
-         y = int(input("b: "))
+         x = int(input("x: ")) 
+         y = int(input("y: "))
          print(divEntier(x,y))
-    except ValueError:
-        print("Veuillez rentrer un nombre entier")
+    except ValueError as err:
+        print(f"Merci de renter un nombre entier: {err}")
