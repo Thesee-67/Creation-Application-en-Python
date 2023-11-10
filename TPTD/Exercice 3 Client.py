f1a4051692be401a437fd081dd7e593e10eedccb
Guittet <og67guittet@gmail.com>
2023-11-10 08:53:37 +0100

    R3.09

--- TPTD/Exercice 3 Client.py
+++ TPTD/Exercice 3 Client.py
@@ -2,10 +2,15 @@ import socket
 if __name__ == '__main__':
     host = "127.0.0.1"
     port = 10000
-    message = "Hello"
+    message = input("Entrez le message à envoyer: ")
     client_socket = socket.socket()
     client_socket.connect((host, port))
     client_socket.send(message.encode()) 
-    reply = client_socket.recv(1024).decode()
-    print("J'ai reçu le message", reply)
-    client_socket.close()
+
+    if message == "bye":
+        print("Demande de déconnexion envoyée au serveur. Fermeture du client.")
+    else:
+        reply = client_socket.recv(1024).decode()
+        print("J'ai reçu le message", reply)
+        client_socket.close()
+        print("fermeture de la coversation")
--- TPTD/Exercice 3 Serveur.py
+++ TPTD/Exercice 3 Serveur.py
@@ -10,9 +10,15 @@ if __name__ == '__main__':
     server_socket.listen(1)
     conn, address = server_socket.accept()
     message = conn.recv(1024).decode()
-    print("J'ai reçus le message", message)
-    conn.send(reply.encode())
-    print("J'ai envoyé le message", reply)
-    conn.close() 
-    server_socket.close()
-    print("je termine la conversation")
+    print("J'ai reçu le message", message)
+
+    if message == "bye":
+        print("Client a demandé de quitter. Attente de la déconnexion du client...")
+        conn.close()
+        server_socket.close()
+    else:
+        conn.send(reply.encode())
+        print("J'ai envoyé le message", reply)
+        conn.close()
+        server_socket.close()
+        print("Je termine la conversation")
